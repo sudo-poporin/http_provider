b703ca2b0c2ccf6232a27d1ca8c26bee2abfa851
Poporin <nahuelballari@gmail.com>
2026-05-23 20:44:42 -0300

    #2: HTTPProvider acepta interceptorsBuilder en constructor

--- test/src/http_provider_test.dart
+++ test/src/http_provider_test.dart
@@ -48,6 +48,21 @@ void main() {
       p.close(force: true);
     });
 
+    test('interceptorsBuilder recibe el Dio interno y registra interceptors', () {
+      Dio? capturedDio;
+      final provider = HTTPProvider(
+        interceptorsBuilder: (dio) {
+          capturedDio = dio;
+          return [
+            RetryInterceptor(dio: dio, maxRetries: 1, initialBackoffMs: 100),
+          ];
+        },
+      );
+
+      expect(capturedDio, isNotNull);
+      expect(provider, isA<HTTPProvider>());
+    });
+
     test('close delega en Dio.close', () {
       when(() => dio.close(force: any(named: 'force'))).thenReturn(null);
 
